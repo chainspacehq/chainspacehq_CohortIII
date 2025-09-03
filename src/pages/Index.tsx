fd401269448da32cb345e04dbad7c7d0cb98df65
Israel Effiong <chainspacehq@gmail.com>
2025-09-03 15:54:32 +0100

    script update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,6 +7,7 @@ import SecondCohort from "@/components/SecondCohort";
 import VolunteerOpportunities from "@/components/VolunteerOpportunities";
 import Footer from "@/components/Footer";
 
+
 const Index = () => {
   return (
     <div className="flex flex-col mx-auto px-3 sm:px-6 lg:px-8 w-full items-center justify-center dark min-h-screen max-w-full lg:max-w-[64rem]">
